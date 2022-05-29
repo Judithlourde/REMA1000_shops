3b650afd56a2e566c44429261c8641e3011c6715
judith lourde <judith.lourde@gmail.com>
2022-05-29 17:05:46 +0200

    Add header background color

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="header">
+    <header role="header" class="header">
         <div>
             <img src="/images/logo-rema1000.png" alt="rema 1000 shop logo">
         </div>
@@ -11,31 +11,23 @@
 </script>
 
 <style>
-   /* Header Section-Navigation-bar */
+   /* Header */
     .header {
         position: fixed;
         top: 0;
         left: 0;
         height: 100px;
         width: 100%;
-        /* background-color: blue; */
+        background-color: #012D97;
         display: flex;
         justify-content: center;
         align-items: center;
-        /* width: 100vw;
-        height: 100px;
-        z-index: 200;
-        padding: 30px 25px;
-        transition: opacity .3s ease-in-out;
-        background-color: rgba(255, 255, 255, 0.07); */
-        /* Got tips from here: https://css-tricks.com/almanac/properties/b/backdrop-filter/ */
-        /* backdrop-filter: saturate(180%) blur(20px);           
-        box-shadow: 0 2px 4px 0 rgb(0 0 0 / 4%), 0 -1px 0 0 rgb(0 0 0 / 8%); */
     }
 
     .header div {
         width: 200px;
         height: 200px;
+        
         display: flex;
         justify-content: center;
         align-items: center;
@@ -43,5 +35,6 @@
 
     .header div img {
         width: 100%;
+        object-fit: cover;
     }
 </style>
